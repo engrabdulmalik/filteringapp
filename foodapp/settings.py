9a06d4b801ba01d6f7b7574b661f78302fea87c8
engrabdulmalik <engrabdulmaliq@gmail.com>
2024-06-01 16:42:51 +0500

    new changes

--- foodapp/settings.py
+++ foodapp/settings.py
@@ -47,7 +47,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3
+    'PAGE_SIZE': 4
 }
 
 
